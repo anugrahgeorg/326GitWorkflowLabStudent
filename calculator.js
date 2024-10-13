619d7092f18cd1d0a889d8e7400164b26c11a453
NikoZBK <nikozbk@gmail.com>
2024-10-13 16:30:46 -0400

    Implement calculateSquare function

--- calculator.js
+++ calculator.js
@@ -3,9 +3,8 @@ function calculateSquareRoot(value) {
   // return the square root of the value
 }
 
-// TODO: Implement square function
 function calculateSquare(value) {
-  return value * value;
+  return value * value; // multiply value by itself instead of using Math
 }
 
 // TODO: FIX reciprocal function
@@ -16,27 +15,26 @@ function calculateReciprocal() {
   return 1 * value;
 }
 function calculateReciprocal(value) {
-    // Hint: Reciprocal is 1/x
-    // return the square root of the value
-    
-    return 1/value;
+  // Hint: Reciprocal is 1/x
+  // return the square root of the value
 
+  return 1 / value;
 }
 
 // TODO: Implement natural log function
 function naturalLog(value) {
   // return the natural log of the value
-    // return the natural log of the value
-    if (value === 0) {
-        return 'Cannot get the natural log of 0';
-    }
-    return Math.log(value)
+  // return the natural log of the value
+  if (value === 0) {
+    return "Cannot get the natural log of 0";
+  }
+  return Math.log(value);
 }
 
 // TODO: Implement sine function
 function sine(value) {
-    // return the sine of the value
-    return Math.sin(value); 
+  // return the sine of the value
+  return Math.sin(value);
 }
 
 // TODO: Implement cosine function
