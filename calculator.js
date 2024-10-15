d55909b91d1b11973940fdc64e6f46353d7ae23d
NikoZBK <nikozbk@gmail.com>
2024-10-15 18:21:04 -0400

    add additional functions to the calculator ui

--- calculator.js
+++ calculator.js
@@ -1,47 +1,40 @@
 // TODO: Fix the square root function not displaying
-function calculateSquareRoot(value) { // George's part
-    if (value < 0) {
-        return "Negative number is not allowed as it cannot be calculated"
-    }
-    
-    return Math.sqrt(value);
-    // return the square root of the value
+function calculateSquareRoot(value) {
+  // George's part
+  // use if condition here
+  if (value < 0) {
+    return "Negative number is not allowed as it cannot be calculated";
+  }
+
+  return Math.sqrt(value);
+  // return the square root of the value
 }
 
-// TODO: Implement square function
 function calculateSquare(value) {
   return value * value;
 }
 
 // TODO: FIX reciprocal function
 
-function calculateReciprocal() {
+function calculateReciprocal(value) {
   // Hint: Reciprocal is 1/x
   // return the square root of the value
-  return 1 * value;
-}
-function calculateReciprocal(value) {
-    // Hint: Reciprocal is 1/x
-    // return the square root of the value
-    
-    return 1/value;
-
+  return 1 / value;
 }
 
 // TODO: Implement natural log function
 function naturalLog(value) {
   // return the natural log of the value
-    // return the natural log of the value
-    if (value === 0) {
-        return 'Cannot get the natural log of 0';
-    }
-    return Math.log(value)
+  if (value === 0) {
+    return "Cannot get the natural log of 0";
+  }
+  return Math.log(value);
 }
 
 // TODO: Implement sine function
 function sine(value) {
-    // return the sine of the value
-    return Math.sin(value); 
+  // return the sine of the value
+  return Math.sin(value);
 }
 
 // TODO: Implement cosine function
@@ -58,4 +51,6 @@ module.exports = {
   calculateSquareRoot,
   calculateSquare,
   calculateReciprocal,
+  sine,
+  naturalLog,
 };
